d84f1353d44c6172de1b415d424d3de8fb9ddac0
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-12-03 14:40:47 +0800

    Finish hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { ChevronDownIcon, Sparkles } from "lucide-react";
+import { ArrowRight, ChevronDownIcon, Play, Sparkles } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -23,6 +23,20 @@ export default function Hero() {
     };
   }, []);
 
+  useEffect(() => {
+    const tabs: TabKey[] = ["App.tsx", "Hero.tsx", "Navbar.tsx"];
+
+    const timeout = setTimeout(() => {
+      setActiveTab((prevTab) => {
+        const currentIndex = tabs.indexOf(prevTab);
+        const nextIndex = (currentIndex + 1) % tabs.length;
+        return tabs[nextIndex];
+      });
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [activeTab]);
+
   const currentFloatingCard = floatingCards[activeTab];
 
   return (
@@ -38,11 +52,48 @@ export default function Hero() {
       <div className="absolute bottom-20 right-4 sm:right-10 w-48 sm:w-72 h-48 sm:h-72 bg-cyan-500/10 rounded-full animate-pulse blur-3xl" />
 
       <div className="max-w-7xl mx-auto text-center relative w-full ">
-        <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 relative">
+        <div className="max-w-7xl mx-auto flex sm:text-left flex-col lg:grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 relative">
           <div>
-            <div className="inline-flex items-center px-3 space-x-2 sm:px-4">
-              <Sparkles />
-              <span>Introducing CodeFlow AI</span>
+            <div className="inline-flex  items-center px-3 space-x-2 sm:px-4 py-2 bg-blue-500/10 border border-blue-500/20 rounded-full  sm:mb-6  animate-in slide-in-from-bottom">
+              <Sparkles className="w-4 h-4 text-blue-400" />
+              <span className="text-xs sm:text-sm text-blue-300">
+                Introducing CodeFlow AI 2.0
+              </span>
+            </div>
+
+            <h1 className="lg:text-5xl xl:text-6xl text-5xl sm:text-3xl md:text-4xl font-semibold mb-4 sm:mb-6 animate-in slide-in-from-bottom-h1 leading-tight lg:text-left ">
+              <span className="block bg-linear-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent mb-1 sm:m-2">
+                Code Faster
+              </span>
+              <span className="block bg-linear-to-r from-blue-400 via-blue-200 to-blue-300 bg-clip-text text-transparent mb-1 sm:m-2">
+                Build Better
+              </span>
+              <span className="block bg-linear-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent mb-1 sm:m-2">
+                With CodeFlow AI{" "}
+              </span>
+            </h1>
+
+            <p
+              className="text-base sm:text-base lg:text-lg text-gray-400 max-w-2xl lg:mx-0 mb-6 sm:mb-8 animate-in slide-in-from-bottom-pill leading-relaxed lg:text-left
+             "
+            >
+              Accelerate your development workflow with intelligent code
+              completion, automated testing, and smart debugging. Ship
+              production ready code 10X faster
+            </p>
+
+            <div className="flex flex-col sm:flex-row   gap-3 sm:gap-4 mb-8 sm:mb-12 animate-in slide-in-from-bottom">
+              <button className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-4 bg-linear-to-b from-blue-600 to-blue-400 rounded-lg font-semibold sm:text-base transition-all hover:scale-102 flex items-center justify-center space-x-2">
+                <span>Start Coding Free</span>
+                <ArrowRight className="w-4 h-4 sm:w-5 sm:h-5 group-hover:translate-x-1 transition-transform duration-300" />
+              </button>
+
+              <button className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-4 bg-white/5 rounded-lg font-semibold sm:text-base transition-all hover:bg-white/10 flex items-center justify-center space-x-2 backdrop-blur-sm border border-white/10">
+                <div className="p-2 bg-white/10 rounded-full group-hover:bg-white/20 duration-200 transition-colors ">
+                  <Play className="w-4 h-4 sm:w-5 sm:h-5 fill-white " />
+                </div>
+                <span>Watch Demo</span>
+              </button>
             </div>
           </div>
 
@@ -50,7 +101,7 @@ export default function Hero() {
             {/* Border */}
             <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border-white/10">
               {/* Screen */}
-              <div className="bg-linear-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] lg:h-[450px] border-white/5 border ">
+              <div className="bg-linear-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] lg:h-[450px] border-white/5 border  ">
                 {/* Screen Header */}
                 <div className="flex items-center justify-between  px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
                   <div className="flex items-center space-x-2">
